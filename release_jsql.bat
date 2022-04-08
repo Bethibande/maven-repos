7177cb88f9b8f5d2a2e90ba56d8aea5b69bef349
maxbb <bethibande@gmail.com>
2022-04-08 14:33:37 +0200

    Released JWebAPI version 1.0.1

--- release_jsql.bat
+++ release_jsql.bat
@@ -3,6 +3,7 @@ set ver=%1
 call mvn install:install-file -DgroupId=de.bethibande -DartifactId=jsql -Dversion=%ver% -Dfile=../JSQL.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
 call git add .
 call git commit -m "Released JSQL version %ver%"
+call git pull origin main
 call git push origin main
 ECHO Finished releasing version %ver%!
 PAUSE
--- release_jweb-api.bat
+++ release_jweb-api.bat
@@ -3,6 +3,7 @@ set ver=%1
 call mvn install:install-file -DgroupId=de.bethibande -DartifactId=jwebapi -Dversion=%ver% -Dfile=../JWebAPI.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
 call git add .
 call git commit -m "Released JWebAPI version %ver%"
+call git pull origin main
 call git push origin main
 ECHO Finished releasing version %ver%!
 PAUSE
