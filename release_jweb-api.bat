a28fa3fac93209c712df7d554d7f3f6a5c2daa0f
maxbb <bethibande@gmail.com>
2022-10-25 17:44:22 +0200

    Released JWebAPI version 2-rc1

--- release_jweb-api.bat
+++ release_jweb-api.bat
@@ -1,6 +1,6 @@
 @echo off
 set ver=%1
-call mvn install:install-file -DgroupId=de.bethibande -DartifactId=jwebapi -Dversion=%ver% -Dfile=../JWebAPI.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
+call mvn install:install-file -DgroupId=de.bethibande -DartifactId=jwebapi -Dversion=%ver% -Dfile=./JWebAPI.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
 call git add .
 call git commit -m "Released JWebAPI version %ver%"
 call git pull origin main
